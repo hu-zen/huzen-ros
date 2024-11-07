cc434d7ef08f2e4de26220ddba19721e46055185
hu-zen <fhadel4677@gmail.com>
2024-11-07 09:50:17 +0800

    Update multiply_three_ints_server.py

--- multiply_three_ints_server.py
+++ multiply_three_ints_server.py
@@ -5,13 +5,13 @@ from beginner_tutorials.srv import MultiplyThreeInts, MultiplyThreeIntsResponse
 import rospy
 
 def handle_multiply_three_ints(req):
-    print("Returning [%s * %s * %s = %s]" % (req.a, req.b, req.c, (req.a * req.b * req.c)))
-    return MultiplyThreeIntsResponse(req.a * req.b * req.c)
+    print("Returning [%s * %s * %s = %s]" % (req.x, req.y, req.z, (req.x * req.y * req.z)))
+    return MultiplyThreeIntsResponse(req.x * req.y * req.z)
 
 def multiply_three_ints_server():
     rospy.init_node('multiply_three_ints_server')
     s = rospy.Service('multiply_three_ints', MultiplyThreeInts, handle_multiply_three_ints)
-    print("Ready to multiply three ints.")
+    print("Ready to calculate the product of three integers.")
     rospy.spin()
 
 if __name__ == "__main__":

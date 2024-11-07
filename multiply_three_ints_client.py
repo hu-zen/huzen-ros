a8fa476a50ffc66cd0164c8d2f11dc426e132411
hu-zen <fhadel4677@gmail.com>
2024-11-07 09:52:25 +0800

    Update multiply_three_ints_client.py

--- multiply_three_ints_client.py
+++ multiply_three_ints_client.py
@@ -3,27 +3,27 @@
 from __future__ import print_function
 import sys
 import rospy
-from beginner_tutorials.srv import MultiplyThreeInts
+from beginner_tutorials.srv import *
 
-def multiply_three_ints_client(a, b, c):
+def multiply_three_ints_client(x, y, z):
     rospy.wait_for_service('multiply_three_ints')
     try:
         multiply_three_ints = rospy.ServiceProxy('multiply_three_ints', MultiplyThreeInts)
-        resp1 = multiply_three_ints(a, b, c)
+        resp1 = multiply_three_ints(x, y, z)
         return resp1.product
     except rospy.ServiceException as e:
         print("Service call failed: %s" % e)
 
 def usage():
-    return "%s [a b c]" % sys.argv[0]
+    return "%s [x y z]" % sys.argv[0]
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
-        a = int(sys.argv[1])
-        b = int(sys.argv[2])
-        c = int(sys.argv[3])
+        x = int(sys.argv[1])
+        y = int(sys.argv[2])
+        z = int(sys.argv[3])
     else:
         print(usage())
         sys.exit(1)
-    print("Requesting %s * %s * %s" % (a, b, c))
-    print("%s * %s * %s = %s" % (a, b, c, multiply_three_ints_client(a, b, c)))
+    print("Requesting %s * %s * %s" % (x, y, z))
+    print("%s * %s * %s = %s" % (x, y, z, multiply_three_ints_client(x, y, z)))
